96b4b670696a75b0f40e2883b4d59e8fe67a1971
Ferhat Ibryamov <ferhatibryamov@Ferhats-MacBook-Air.local>
2026-03-28 20:09:59 +0200

    Fixes

--- app/[url]/route.tsx
+++ app/[url]/route.tsx
@@ -1,26 +1,27 @@
 // app/[url]/route.ts
 import prisma from "@/lib/prisma"
+import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(
-  req: Request,
-  { params }: { params: { url: string } }
+  req: NextRequest,
+  context: { params: { url: string } }
 ) {
-  const shortCode = params.url
+  const shortCode = context.params.url
 
   const record = await prisma.url.findUnique({
     where: { shortCode },
   })
 
   if (!record) {
-    return new Response("URL not found", { status: 404 })
+    return new NextResponse("URL not found", { status: 404 })
   }
 
-  // Make sure URL has http or https
+  // Ensure URL has http or https
   let destination = record.originalUrl
   if (!/^https?:\/\//i.test(destination)) {
     destination = `https://${destination}`
   }
 
   // Redirect
-  return Response.redirect(destination, 302)
+  return NextResponse.redirect(destination)
 }
